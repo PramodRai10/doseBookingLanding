ffe84412b98742c7af58e5c5841c5ca81efd85ec
PramodRai10 <p10pramodr@gmail.com>
2021-04-02 19:46:02 +0530

    Add Keys 2

--- src/FormDetails.js
+++ src/FormDetails.js
@@ -5,13 +5,12 @@ import "./FormDetails.css";
 function FormDetails(props) {
   // Config variables
 
-  console.log(process.env);
   const SPREADSHEET_ID = process.env.REACT_APP_SPREADSHEET_ID;
   const SHEET_ID = process.env.REACT_APP_SHEET_ID;
   const CLIENT_EMAIL = process.env.REACT_APP_GOOGLE_CLIENT_EMAIL;
-  const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY.replace(/\\n/g, '\n');
-  console.log(typeof(SPREADSHEET_ID));
-  console.log(typeof(SHEET_ID));
+  const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY;
+
+  console.log(SPREADSHEET_ID, SHEET_ID, CLIENT_EMAIL, PRIVATE_KEY);
 
   const [data, setData] = useState({
     Name: '',
@@ -29,7 +28,7 @@ function FormDetails(props) {
     try {
       await doc.useServiceAccountAuth({
         client_email: CLIENT_EMAIL,
-        private_key: PRIVATE_KEY,
+        private_key: PRIVATE_KEY.replace(/\\n/g, '\n'),
       });
       // loads document properties and worksheets
       await doc.loadInfo();
